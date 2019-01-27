ef64ff2d33b2132df9e7a248ea647bb9d91659a8
Binod <binod@Binitas-MacBook-Air.local>
2019-01-27 12:55:35 -0700

    vampire mongoose hw done

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -32,42 +32,42 @@ mongoose.connection.on("disconnected", () => {
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
-Vampire.collection.insertMany(vampireList, (err, data) => {
-    console.log("vampire list added");
-    mongoose.connection.close();
-});
+// Vampire.collection.insertMany(vampireList, (err, data) => {
+//     console.log("vampire list added");
+//     mongoose.connection.close();
+// });
 
 
 // ### Add some new vampire data
 
-Vampire.create({
-    name: "Ravana",
-    eye_color: "black",
-    dob: new Date(-1200, 08, 17),
-    loves: ["kidnapping", "gold", "medidation"],
-    victims: 10000,
-}, (err, vampire) => {
-    if(err){
-        console.log(err);    
-    }else {
-        console.log(vampire);
-    }
-});
+// Vampire.create({
+//     name: "Ravana",
+//     eye_color: "black",
+//     dob: new Date(-1200, 08, 17),
+//     loves: ["kidnapping", "gold", "medidation"],
+//     victims: 10000,
+// }, (err, vampire) => {
+//     if(err){
+//         console.log(err);    
+//     }else {
+//         console.log(vampire);
+//     }
+// });
 
-Vampire.create({
-    name: "Lamia",
-    eye_color: "purple",
-    dob: new Date(1450, 03, 21),
-    loves: ["kicking ass", "hurting others", "playing xbox"],
-    victims: 1220,
-    location: "Denver",
-}, (err, vampire) => {
-    if(err){
-        console.log(err);    
-    }else {
-        console.log(vampire);
-    }
-});
+// Vampire.create({
+//     name: "Lamia",
+//     eye_color: "purple",
+//     dob: new Date(1450, 03, 21),
+//     loves: ["kicking ass", "hurting others", "playing xbox"],
+//     victims: 1220,
+//     location: "Denver",
+// }, (err, vampire) => {
+//     if(err){
+//         console.log(err);    
+//     }else {
+//         console.log(vampire);
+//     }
+// });
 
 
 
@@ -76,95 +76,211 @@ Vampire.create({
 /////////////////////////////////////////////////
 // ### Select by comparison
 
-Vampire.find({gender: "f"}, (err, matchFound) => {
-    if(err){
-        console.log(err);
-    }else{
-        console.log(matchFound);
-        mongoose.connection.close();
-    }
-})
+// Vampire.find({gender: "f"}, (err, matchFound) => {
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(matchFound);
+//         mongoose.connection.close();
+//     }
+// })
 
-Vampire.find({victims: {$gt: 1000}}, (err, matchFound) => {
-    if(err){
-        console.log(err);
-    }else{
-        console.log(matchFound);
-        mongoose.connection.close();
-    }
-});
+// Vampire.find({victims: {$gt: 1000}}, (err, matchFound) => {
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(matchFound);
+//         mongoose.connection.close();
+//     }
+// });
 
 
-Vampire.find({victims: {$lt: 250}}, (err, matchFound) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(matchFound);
-        mongoose.connection.close();
-    }
-});
+// Vampire.find({victims: {$lt: 250}}, (err, matchFound) => {
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(matchFound);
+//         mongoose.connection.close();
+//     }
+// });
 
-Vampire.find({eye_color: "blue"}, (err, matchFound) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(matchFound);
-        mongoose.connection.close();
-    }
-});
+// Vampire.find({eye_color: "blue"}, (err, matchFound) => {
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(matchFound);
+//         mongoose.connection.close();
+//     }
+// });
 
-Vampire.find({loves: "blood"}, (err, matchFound) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(matchFound);
-        mongoose.connection.close();
-    }
-});
+// Vampire.find({loves: "blood"}, (err, matchFound) => {
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(matchFound);
+//         mongoose.connection.close();
+//     }
+// });
 
-Vampire.find({victims: {$in: [120, 500, 1000]}}, (err, matchFound) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(matchFound);
-        mongoose.connection.close();
-    }
-})
+// Vampire.find({victims: {$in: [120, 500, 1000]}}, (err, matchFound) => {
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(matchFound);
+//         mongoose.connection.close();
+//     }
+// })
 
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
 
-Vampire.find({victims: {$exists: false} }, (err,found)=>{
-    if(err){
-        console.log(err);
-            }else{
-            console.log(found);
-            mongoose.connection.close();
-        }
-    });
+// Vampire.find({victims: {$exists: false} }, (err,found)=>{
+//     if(err){
+//         console.log(err);
+//             }else{
+//             console.log(found);
+//             mongoose.connection.close();
+//         }
+//     });
 
 
-    Vampire.find(
-            {victims: {$exists: true, $gt: 1000} }, (err,found)=>{
-                if(err){
-                    console.log(err);
-                }
-                console.log(found);
-                mongoose.connection.close();
-            }
-        )
+//     Vampire.find(
+//             {victims: {$exists: true, $gt: 1000} }, (err,found)=>{
+//                 if(err){
+//                     console.log(err);
+//                 }
+//                 console.log(found);
+//                 mongoose.connection.close();
+//             }
+//         )
 /////////////////////////////////////////////////
 // ### Select with OR
+
+// Vampire.find({$or: [{location: 'Auvergne, France'},{location: "New Orleans, Louisiana, US"}]}, (err, vampire)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log(`Or locations`, vampire);
+//     }
+// });
+
+// Vampire.find({$or: [{loves: 'brooding'},{loves: "being tragic"}]}, (err, vampire)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log(`Or loves`, vampire);
+//     }
+// });
+
+// Vampire.find({$or: [{victims: {$gt: 1000}},{loves: "marshmallows"}]}, (err, vampire)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log(`1000 victims or loves marshmallows`, vampire);
+//     }
+// });
+
+// Vampire.find({$or: [{hair_color: 'red'},{eye_color: "green"}]}, (err, vampire)=>{
+//     if (err) {
+//         console.log(err);
+//     }else{
+//         console.log(`red hair or green eyes`, vampire);
+//     }
+// });
 
 /////////////////////////////////////////////////
 //### Select objects that match one of several values
 
+
+// Vampire.find({$or: [{"loves":"frilly shirtsleeves"}, {"loves": "frilly collars"}]}, (err, data) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("==========SON===========");
+//         console.log(data);
+//     }
+// })
+// Vampire.find({"loves":"brooding"}, (err, data) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// })
+// Vampire.find({$or: [{"loves":"appearing innocent"}, {"loves": 'trickery'},{"loves": "lurking in rotting mansions"},{"loves":"R&B music"}]}, (err, data) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// })
+// Vampire.find({$and: [{"loves":"fancy cloaks"}, {$nin: [{"loves": 'top hats'},{"loves": "virgin blood"}]}]}, (err, data) => {
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// })
+
 /////////////////////////////////////////////////
 //### Negative Selection
+
+// Vampire.find(
+//     { loves: "ribbons",
+//     eye_color: {$ne:"brown"} },
+//     (err, vampires) => {
+//         console.log("LOVE RIBBON", vampires); 
+// 	}
+// );
+
+// Vampire.find(
+//     { location: { $ne:"Rome"},
+//     eye_color: {$ne:"brown"} },
+//     (err, vampires) => {
+//         console.log("NOT ROME", vampires); 
+// 	}
+// );
+
+// Vampire.find(
+//     { $or: { $ne: [{loves:"fancy cloaks"},
+//     {loves:"frilly shirtsleeves"},
+//     {loves:"appearing innocent"},
+//     {loves:"brooding"},
+//     {loves:"being tragic"},
+//     ]}},
+//     (err, vampires) => {
+//         console.log("DOESNT LOVE", vampires); 
+// 	}
+// );
+ 
+// Vampire.find(
+//     { victims: {$lte:200} },
+//     (err, vampires) => {
+//         console.log("KILLED <= 200", vampires); 
+// 	}
+// );
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## REPLACE
+
+
+// Vampire.update(
+//     { name: "Claudia" },
+//     { $set: { name:"Eve" , portrayed_by: "Tilda Swinton" } },
+//     (err, vampires) => {
+//         console.log("RENAME EVE", vampires); 
+// 	}
+// );
+
+// Vampire.findOneAndUpdate(
+//     { gender: "m" },
+//     { $set: { name:"Guy Man" , is_actually: "were-lizard" } },
+//     (err, vampires) => {
+//         console.log("MAKE FIRST MALE WERE-LIZARD", vampires); 
+// 	}
+// );
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -173,6 +289,22 @@ Vampire.find({victims: {$exists: false} }, (err,found)=>{
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // ## REMOVE
+
+// Vampire.findOne(
+//     { hair_color: "brown" },
+//     (err, vampires) => {
+//         post.remove(function(error){
+//         });
+//         console.log("REMOVE ONE BROWN HAIRED", vampires); 
+// 	}
+// );
+
+// Vampire.remove(
+//     { eye_color: "blue" },
+//     (err, vampires) => {
+//         console.log("REMOVED ALL BLUE EYES", vampires); 
+// 	}
+// );
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
